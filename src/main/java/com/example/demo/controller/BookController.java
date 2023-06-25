df6ed74b6ac7fe3603454a89d58ce06349e33c8e
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2023-06-25 17:20:14 +0530

    aded response util content

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -1,5 +1,24 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.BookDto;
+import com.example.demo.service.BookService;
+import org.apache.tomcat.util.http.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/book")
 public class BookController {
 
+    @Autowired
+    private BookService service;
+
+    @PostMapping
+    public ResponseEntity saveBook(@RequestBody BookDto dto){
+        service.saveBook(dto);
+        return new ResponseUtil(200, "Admin added Successfully", null);
+    }
 }
