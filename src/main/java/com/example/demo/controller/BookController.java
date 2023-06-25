61a603c4806dabd703ce47491d87b821fc97bce2
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2023-06-25 17:33:56 +0530

    updated saveBookController content

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -2,11 +2,12 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.BookDto;
 import com.example.demo.service.BookService;
-import org.apache.tomcat.util.http.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import util.ResponseUtil;
 
 @RestController
 @CrossOrigin
@@ -16,8 +17,14 @@ public class BookController {
     @Autowired
     private BookService service;
 
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil getAllBooks(){
+        return  new ResponseUtil(200, "OK",service.getAllBook());
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public ResponseEntity saveBook(@RequestBody BookDto dto){
+    public ResponseUtil saveBook(@RequestBody BookDto dto){
         service.saveBook(dto);
         return new ResponseUtil(200, "Admin added Successfully", null);
     }
